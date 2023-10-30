be7c8434481d5ae1a611f9c33761cb0136a83c40
robertnelson-co <hello@robertnelson.co>
2023-10-30 18:14:18 -0400

    finishing root file update

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,11 +1,19 @@
-from flask import ( Blueprint, render_template )
-import json
+from flask import ( Blueprint, render_template ) 
+import json 
+
 pets = json.load(open('pets.json'))
-bp = Blueprint('pet', __name__, url_prefix='/pets')
+print(pets)
 
-#print (pets)
+bp = Blueprint('pet', __name__, url_prefix="/pets")
 
+@bp.route('/')
+def index(): 
+    return render_template('pets/index.html', pets=pets)
 
-@bp.route("/")
-def index():
-    return render_template('index.html', pets=pets)
+@bp.route('/show')
+def new():
+    return render_template('pets/show.html')
+
+#@bp.route('/show/:id')
+#def new():
+#    return render_template('pets/show.html')
